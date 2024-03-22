cbee594c4c58a216a8e3ce6ea0fa83ecb23237c7
josh2816 <164451084+josh2816@users.noreply.github.com>
2024-03-22 16:39:31 -0700

    Update book.java

---
+++ book.java
@@ -0,0 +1,15 @@
+public static class Book{
+	final static String title;
+	final static String author;
+	final static String datePublished;
+	final static String publisher;
+	final static String summary;
+	final static String type;
+	final static String location;
+	final static String language;
+	String condition;
+	final static String genre;
+	final static String subjects;
+	int copies; 
+	//setters and getters needed
+}
