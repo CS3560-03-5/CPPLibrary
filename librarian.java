58323ede35fb4f10ed2c0aa6f984ce13f6726604
austinP2 <119561083+austinP2@users.noreply.github.com>
2024-03-22 16:52:48 -0700

    Update librarian.java

--- librarian.java
+++ librarian.java
@@ -1,9 +1,9 @@
 public static class{
-  final static String name;
-  final static String desc;
-  final static String email;
-  final static String phoneNum;
-  final static String subSpec;
-  final static String guides;
+        final static String name;
+        final static String desc;
+        final static String email;
+        final static String phoneNum;
+        final static String subSpec;
+        final static String guides;
   
 }
