8073c2c2fed1be4c48920dfa0411cefe63341a62
JahdonFaulcon <149533957+JahdonFaulcon@users.noreply.github.com>
2024-03-22 17:56:35 -0700

    Update student.java

--- student.java
+++ student.java
@@ -2,7 +2,7 @@ public class student {
     private final int broncoId = 0;
     private final String name = "";
     private final String email = "";
-    public double fines;
+    public float fines;
     public request requests[];
     public book borrowedBooks[];
 
