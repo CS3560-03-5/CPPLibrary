3d981c68e7e1ec0939bfd1eaa04dd75133ec2d71
JahdonFaulcon <149533957+JahdonFaulcon@users.noreply.github.com>
2024-03-22 17:53:42 -0700

    Create student.java

---
+++ student.java
@@ -0,0 +1,9 @@
+public class student {
+    private final int broncoId = 0;
+    private final String name = "";
+    private final String email = "";
+    public double fines;
+    public request requests[];
+    public book borrowedBooks[];
+
+}
