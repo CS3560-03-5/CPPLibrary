c532a231740c8e8a56e568d113c973a942d3b773
Brandon Chang <brandon.ch2ng@gmail.com>
2024-03-22 21:26:19 -0700

    fee class changes

--- fee.java
+++ fee.java
@@ -1,4 +1,15 @@
 public class fee {
-    float amount;
-    int dueDate;
+    private float amount;
+    private int dueDateInDays;
+
+    public fee(float amount, int dueDateInDays) {
+        this.amount = amount;
+        this.dueDateInDays = dueDateInDays;
+    }
+
+    public float getAmount() { return amount; }
+    public void setAmount(float amount) { this.amount = amount; }
+    public int getDueDateInDays() { return dueDateInDays; }
+    public void setDueDateInDays(int dueDateInDays) { this.dueDateInDays = dueDateInDays; }
 }
+
