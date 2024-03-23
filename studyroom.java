0a6401c3aeb474e832fd4b9e7ae7a37fdf250646
josh2816 <164451084+josh2816@users.noreply.github.com>
2024-03-22 18:14:58 -0700

    Update studyroom.java

--- studyroom.java
+++ studyroom.java
@@ -1,6 +1,20 @@
 class studyroom {
-    //fields
-    public boolean status;
-    public String location;
+	//fields
+	public boolean status;
+	public String location;
+	public studyroom(){ //constructor
 
+	}
+	public void reserveStudyRoom(studyroom studyroom, int duration){
+		
+	}
+	public void cancelReservation(studyroom studyroom){
+		
+	}
+	public void updateStatus(studyroom studyroom, boolean status){
+		this.status=status;
+	}
+	public boolean getStatus(studyroom studyroom){
+		return this.status;
+	}
 }
