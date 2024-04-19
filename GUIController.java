19ee93575ff12b327a67f4bbb17001df0d68fb1f
JahdonFaulcon <149533957+JahdonFaulcon@users.noreply.github.com>
2024-04-19 15:32:21 -0700

    Update GUIController.java

--- GUIController.java
+++ GUIController.java
@@ -1,3 +1,31 @@
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
 public class GUIController {
+
     
+    
+    @FXML
+    private TextField txtFieldName;
+
+    private Stage mainWindow;
+
+    public void setMainWindow(Stage mainWindow) {
+        this.mainWindow = mainWindow;
+    }
+
+    @FXML
+    void onBtnClick(ActionEvent event) {
+        String title = txtFieldName.getText();
+        mainWindow.setTitle(title);
+
+
+    }
+
 }
+
+    
+
