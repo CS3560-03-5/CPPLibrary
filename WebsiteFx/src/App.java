508cfa62624bbba060cd251458c76069fe6791ec
JahdonFaulcon <149533957+JahdonFaulcon@users.noreply.github.com>
2024-04-22 15:45:20 -0700

    organized things in separate files

--- WebsiteFx/src/App.java
+++ WebsiteFx/src/App.java
@@ -19,6 +19,8 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 
+import pages.FrontPage;
+
 public class App extends Application{
     @Override
     public void start(Stage primaryStage) {
@@ -32,57 +34,10 @@ public class App extends Application{
             bp.setMaxWidth(bounds.getMinX());
             bp.setMaxHeight(bounds.getMinY());
            
-            //menu items
-            MenuButton studentOptions = new MenuButton("Students");
-            MenuItem borrowedBooks = new MenuItem("Borrowed Books");
-            MenuItem fees = new MenuItem("Fees");
-            MenuItem requests = new MenuItem("Requests");
-            MenuItem logIn = new MenuItem("Log In");
-            
-            studentOptions.getItems().addAll(borrowedBooks, requests, fees, logIn);
-
-            VBox studentOptionsBox = new VBox(studentOptions);
-            studentOptionsBox.setPadding(new Insets(0, 0, 0, 100));
-
-            Label home = new Label("Home");
-            VBox homeBox = new VBox(home);
-            homeBox.setAlignment(Pos.CENTER_RIGHT);
-            homeBox.setPadding(new Insets(0, 100, 0, 0));
-            
-            
-            Label books = new Label("Books");
-           
-            MenuButton librarian = new MenuButton("Librarians");
-            MenuItem chat = new MenuItem("24/7 Chat");
-            MenuItem guides = new MenuItem("Guides");
-            librarian.getItems().addAll(chat, guides);
-
-            Label studyRooms = new Label("StudyRooms");
-
-            HBox middleOptions = new HBox(books, librarian, studyRooms);
-            middleOptions.setSpacing(50);
-           
-
-            HBox topOfPane = new HBox(homeBox, middleOptions, studentOptionsBox);
-            topOfPane.setAlignment(Pos.CENTER);
-           
-            bp.setTop(topOfPane);
-
-            Label searchPrompt = new Label("Search");
-            TextField searchField = new TextField("Enter a Book, Author, or Publisher");
-            HBox search = new HBox(searchPrompt, searchField);
-            search.setSpacing(10);
-            search.setAlignment(Pos.CENTER);
-
-            Button libraryChat = new Button("Chat with a Librarian");
-            Button studyRoom = new Button("Reserve a Study Room");
-            
-            VBox frontPage = new VBox(search, libraryChat, studyRoom);
-            frontPage.setSpacing(50);
-            frontPage.setPadding(new Insets(0,0,0,0));
-            frontPage.setAlignment(Pos.CENTER);
-
-            bp.setCenter(frontPage);
+            //opens website on Front Page by default 
+            bp.setTop(FrontPage.topOfPane);
+            bp.setCenter(FrontPage.frontPage);
+            FrontPage.settings(primaryStage);
 
             primaryStage.setTitle("CPP Library");
             primaryStage.setScene(new Scene(bp));
