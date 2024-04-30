7aa6d885abb6aa57b84b58ec0f5265cb62ce9345
josh2816 <164451084+josh2816@users.noreply.github.com>
2024-04-29 20:13:41 -0700

    Create LoginPage.java

---
+++ WebsiteFx/src/pages/LoginPage.java
@@ -0,0 +1,19 @@
+package pages;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Orientation;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.control.Separator;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class LoginPage {
+    public static Label title = new Label("Cal Poly Pomona Library");
+    public static Label usernamePrompt = new Label("Username:");
+    public static Label passwordPrmpt = new Label("Password:");
+}
