472d165e85e0c27fc28d59444eaab59cafe66d9f
josh2816 <164451084+josh2816@users.noreply.github.com>
2024-05-05 17:24:30 -0700

    Revert "Update App.java"
    This reverts commit b6a9525c4ce9f952ffb4bb6cd7a3c0d70a90611b.

--- WebsiteFx/src/App.java
+++ WebsiteFx/src/App.java
@@ -331,7 +331,7 @@ public class App extends Application{
 
                 
             });
-            AccountNotFoundPagePage.backToLogin.setOnMouseEntered(event -> {
+            AccountNotFoundPagePage.back.setOnMouseEntered(event -> {
                 ProductPage.backToLogin.setStyle("-fx-text-fill: blue; -fx-underline: true;");
             });
             AccountNotFoundPagePage.back.setOnMouseExited(event -> {
